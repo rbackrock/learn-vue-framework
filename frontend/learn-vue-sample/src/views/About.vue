5c5da316f6e0bb194192f411ec0a3e42ada49d80
yhuan <cokekokotea@gmail.com>
2020-04-16 10:59:57 +0800

    update

--- frontend/learn-vue-sample/src/views/About.vue
+++ frontend/learn-vue-sample/src/views/About.vue
@@ -2,6 +2,8 @@
   <div>
     <h3>关于页面</h3>
     <button @click="handleChange">演示 Vuex 我要变标题</button>
+    <img src="@/assets/logo.png" alt="" style="display: block;">
+    <div class="login"></div>
   </div>
 </template>
 
@@ -16,3 +18,11 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+  .login {
+    width 200px
+    height 200px
+    background url(../assets/logo.png)
+  }
+</style>
--- frontend/learn-vue-sample/vue.config.js
+++ frontend/learn-vue-sample/vue.config.js
@@ -6,5 +6,12 @@ module.exports = {
         changeOrigin: true
       }
     }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+        .use('url-loader')
+          .loader('url-loader')
+          .tap(options => Object.assign(options, { limit: 10240 }))
   }
 }
